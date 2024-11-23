663d9d3eb3f32b0f9f171d5e260435b86e98523b
JTeran28 <84333695+JTeran28@users.noreply.github.com>
2024-11-23 23:11:23 +0100

    Create teenHireIntegration.js

---
+++ teenhireintegration.js
@@ -0,0 +1,3 @@
+// Replace with your actual API key
+const API_KEY = 'sk-JyLzDisKpCoh8k7Orq0Rlu4ndt0b2EYHz7I10QIZSdg==';
+const BASE_URL = 'https://api.teen4test.online'; // Use 'https://api.teenhire.co' for production
